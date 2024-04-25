be74d652848b30399b0075d8e0872bdd018875a1
kirackvijay <113379470+kirackvijay@users.noreply.github.com>
2024-04-25 11:45:14 +0530

    Changes

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import path from "path";
 
 const app = express();
-const port = process.env.PORT || 4010;
+const port = process.env.PORT || 4000;
 
 app.use(express.static(path.resolve("./public")));
 
